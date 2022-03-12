ca84877efe3e526fbc149fefecb9e8a63da06a56
DrAmyCMartin <100963001+DrAmyCMartin@users.noreply.github.com>
2022-03-12 15:50:58 -0500

    RealInvesting

---
+++ story_content/6HM2rXaRjzQ_captions.js
@@ -0,0 +1,1 @@
+window.globalProvideData('caption', '{"data":"WEBVTT%0D%0AKind:%20captions%0D%0ASource:%20Articulate%20Closed%20Captions%20Editor%0D%0ASource%20Version:%206.61.27106.0%0D%0A%0D%0A00:00:00.032%20--%3E%2000:00:03.212%0D%0AThat\u0027s%20right.%20Click%20the%20Continue%20button%20to%20learn%20more.%0D%0A%0D%0A"}')
